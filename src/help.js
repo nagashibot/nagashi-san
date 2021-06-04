9387986ad23d78b519dfba056c0eb4840500947b
nagashibot <80216329+nagashibot@users.noreply.github.com>
2021-06-03 21:21:03 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,88 @@
+const help = (prefix) => {
+	return `
+║   ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+║╭─────────────────────
+║卐™- ༷￫ ${prefix}bv [0/1]
+║卐™- ༷￫ ${prefix}bemvindo [0/1]
+║卐™- ༷￫ ${prefix}boasvindas [0/1]
+║├────> desativar/ativa bv
+║├─────────────────────
+║卐™- ༷￫ ${prefix}add 55xxx
+║卐™- ༷￫ ${prefix}adicionar 55xxx
+║├────> add menbro
+║├─────────────────────
+║卐™- ༷￫ ${prefix}ban @membro
+║卐™- ༷￫ ${prefix}banir @membro
+║├────> remover membro
+║├─────────────────────
+║卐™- ༷￫ ${prefix}adm @membro
+║卐™- ༷￫ ${prefix}promover @membro
+║├────> promover a ADM
+║├─────────────────────
+║卐™- ༷￫ ${prefix}tiraadm @adm
+║卐™- ༷￫ ${prefix}promover @adm
+║├────> rebaixar a membro
+║├─────────────────────
+║卐™- ༷￫ ${prefix}abrir
+║卐™- ༷￫ ${prefix}fechar
+║├────> abrir/fechar grupo 
+║├─────────────────────
+║卐™- ༷￫ ${prefix}setname seu txt
+║卐™- ༷￫ ${prefix}mudarnome seu txt
+║├────> troca nome do grupo
+║├─────────────────────
+║卐™- ༷￫ ${prefix}setdesc seu txt
+║卐™- ༷￫ ${prefix}mudardesc seu txt
+║├────> troca descrição
+║├─────────────────────
+║卐™- ༷￫ ${prefix}mudarfoto
+║├────> mudar foto do grupo
+║├─────────────────────
+║卐™- ༷￫ ${prefix}marcar
+║├────> marcação
+║├─────────────────────
+║卐™- ༷￫ ᎠϴᏔΝᏞϴᎪᎠ/ᏞᏆΝᏦ
+║├─────────────────────
+║卐™- ༷￫ ${prefix}ytmp4 link
+║卐™- ༷￫ ${prefix}ytvideo link
+║卐™- ༷￫ ${prefix}ytbuscar link
+║卐™- ༷￫ ${prefix}ytbaixa link
+║├────> baixa vídeo do YouTube
+║├─────────────────────
+║卐™- ༷￫ ${prefix}play nome da música
+║卐™- ༷￫ ${prefix}msc nome da música
+║├────> baixa música do YouTube
+║├─────────────────────
+║卐™- ༷￫ᎷᏆᎠᏆᎪ/ϴႮͲᎡϴՏ
+║├─────────────────────
+║卐™- ༷￫ ${prefix}f
+║卐™- ༷￫ ${prefix}figu 
+║卐™- ༷￫ ${prefix}sticker 
+║├────> fazer fig cm foto/gif
+║├─────────────────────
+║卐™- ༷￫ ${prefix}f1
+║├────> fazer fig quadrada foto/gif
+║├─────────────────────
+║卐™- ༷￫ ${prefix}sttc seu txt ou emoji
+║├────> sttc  cm seu texto ou emoji
+║├─────────────────────
+║卐™- ༷￫ ${prefix}tts seu txt
+║卐™- ༷￫ ${prefix}gtts seu txt
+║卐™- ༷￫ ${prefix}audio seu txt
+║├────> converter txt em áudio
+║├─────────────────────
+║卐™- ༷￫ ${prefix}toimg marca fig
+║卐™- ༷￫ ${prefix}converter marca fig
+║├────> converter fig em foto
+║├─────────────────────
+║卐™- ༷￫ ${prefix}limpar
+║├────> limpa zap do bot
+║├─────────────────────
+║卐™- ༷￫ ${prefix}setprefix
+║├────> troca sigla de comandos
+║└─────────────────────
+╚══════════════════════
+`
+}
+
+exports.help = help
